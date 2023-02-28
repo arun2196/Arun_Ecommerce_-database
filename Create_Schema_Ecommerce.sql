97c532a7adbdb85e994e315b3b3236a2b9519140
arun2196 <arun.tiwari2196@gmail.com>
2023-02-28 16:36:11 +0530

    LAB 4 - DBMS

---
+++ Create_Schema_Ecommerce.sql
@@ -0,0 +1,2 @@
+create schema ecommerce;
+USE ecommerce;
---
+++ Question_9.sql
@@ -0,0 +1,3 @@
+# Q9) Create a stored procedure to display supplier id, name, rating and Type_of_Service. For Type_of_Service, If rating =5, print “Excellent
+# Service”,If rating >4 print “Good Service”, If rating >2 print “Average Service” else print “Poor Service”.
+call get_supplier_rating_info ();
